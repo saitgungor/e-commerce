7c00d54d2e27b163b65de1db23a81badd1752e0d
Osman Sait Güngör <sait@bronixengineering.com>
2023-11-26 18:40:59 +0300

    implementing notification bar

---
+++ components/NotificationBar/index.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { notificationItems } from "@/constants/notificationItems";
+import { useEffect, useState } from "react";
+
+const NotificationBar = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const nextNotificationHandler = () => {
+    setActiveIndex((prev) => (prev === notificationItems.length - 1 ? 0 : prev + 1));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextNotificationHandler();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center bg-[#ffbba0] text-[#0b0a18] w-full h-6 font-medium overflow-hidden text-sm whitespace-nowrap">
+      {notificationItems.map((item, index) => (activeIndex === index ? <p key={item}>{item}</p> : null))}
+    </div>
+  );
+};
+
+export default NotificationBar;
---
+++ constants/notificationItems.js
@@ -0,0 +1,6 @@
+export const notificationItems = [
+  "14:00'a kadar verilen siparişleriniz aynı gün kargoda.",
+  "60 gün içerisinde koşulsuz iade hakkı!",
+  "500₺ üzeri kargo ücretsiz.",
+  "Yeni üyelere özel %10 Hoş geldin indirimi!",
+];
